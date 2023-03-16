8b31dea9d24395c7e67e8843885c2f3bde6c3d72
Georgy Savva <georgy.savva@gmail.com>
2023-03-16 17:57:09 -0400

    init project

---
+++ pages/users.tsx
@@ -0,0 +1,10 @@
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+
+export default function Users(props: AppProps) {
+  return (
+    <div>
+      <h1>Users</h1>
+    </div>
+  );
+}
