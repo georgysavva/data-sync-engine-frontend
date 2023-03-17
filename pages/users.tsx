f344393aa52f5cbdfb2fc80f1e99ba565aa755b7
Georgy Savva <georgy.savva@gmail.com>
2023-03-17 09:05:20 -0400

    implement users page

--- pages/users.tsx
+++ pages/users.tsx
@@ -1,10 +1,28 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { User } from "@/entities/user.js";
+import axios from "axios";
+import { GetServerSideProps } from "next";
 
-export default function Users(props: AppProps) {
+export default function Users(props: { users: User[] }) {
   return (
     <div>
-      <h1>Users</h1>
+      <h1>All users Accounts</h1>
+      <ul>
+        {props.users.map((user) => (
+          <li key={user.id}>
+            {user.id} {user.service}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { data: users }: { data: User[] } = await axios.get(
+    `${process.env.BACKEND_API_BASE_URL}/users`
+  );
+
+  return {
+    props: { users },
+  };
+};
